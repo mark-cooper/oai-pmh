641e12ec07f35c447fe0a0b08e098cac8b7a5698
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-11-27 14:36:47 -0800

    More tests, future chrono maybe

--- src/client/query.rs
+++ src/client/query.rs
@@ -26,6 +26,7 @@ pub struct GetRecordArgs {
     pub metadata_prefix: String,
 }
 
+// TODO: ISO8601 (consider chrono in the future for stricter from/until handling)
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ListRecordsArgs {
@@ -45,7 +46,7 @@ pub struct ResumableArgs {
 mod tests {
     use crate::{
         Verb,
-        client::query::{GetRecordArgs, Query},
+        client::query::{GetRecordArgs, ListRecordsArgs, Query},
     };
 
     #[test]
@@ -57,6 +58,30 @@ mod tests {
         };
 
         let query = Query::new(Verb::GetRecord, args);
+        let from_qs = serde_qs::from_str(q).unwrap();
+        assert_eq!(query, from_qs);
+    }
+
+    #[test]
+    fn construct_identify_query() {
+        let q = "verb=Identify";
+
+        let query = Query::new(Verb::Identify, ());
+        let from_qs = serde_qs::from_str(q).unwrap();
+        assert_eq!(query, from_qs);
+    }
+
+    #[test]
+    fn construct_list_records_query() {
+        let q = "verb=ListRecords&metadataPrefix=oai_ead&from=2000-01-01&until=2025-01-01";
+        let args = ListRecordsArgs {
+            metadata_prefix: "oai_ead".into(),
+            from: Some("2000-01-01".into()),
+            until: Some("2025-01-01".into()),
+            set: None,
+        };
+
+        let query = Query::new(Verb::ListRecords, args);
         let from_qs = serde_qs::from_str(q).unwrap();
         assert_eq!(query, from_qs);
     }
