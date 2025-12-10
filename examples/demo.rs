3734d29073f9503ac50f3781fe9471500c573022
Mark Cooper <551470+mark-cooper@users.noreply.github.com>
2025-12-09 20:57:18 -0800

    Copy demo

---
+++ examples/demo.rs
@@ -0,0 +1,17 @@
+use anyhow::Result;
+use oai_pmh::{Client, ListRecordsArgs};
+
+fn main() -> Result<()> {
+    let client = Client::new("https://demo.archivesspace.org/oai")?;
+
+    let response = client.identify()?;
+    println!("{:?}", response.payload);
+
+    let args = ListRecordsArgs::new("oai_dc");
+    for response in client.list_records(args)? {
+        println!("{:?}", response);
+        break;
+    }
+
+    Ok(())
+}
